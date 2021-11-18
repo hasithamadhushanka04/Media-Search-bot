a4c8bb4f12c1dfadd6cfc63e42a464f5b29ffce6
Hasitha Madhushanka <90751622+hasithamadhushanka04@users.noreply.github.com>
2021-11-18 18:04:43 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -90,8 +90,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('සොයන්න 🔎', switch_inline_query_current_chat=query),
+            InlineKeyboardButton('Join Channel', url='https://t.me/CloudUpdateslk')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
